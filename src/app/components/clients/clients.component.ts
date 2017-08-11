62a0b148fb26dcb8e339e75df6bfc8c3d91dbb32
Luis Pintado <luchopintado@gmail.com>
2017-08-11 18:17:20 -0500

    Clients table component

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -11,17 +11,28 @@ import { Client } from '../../models/Client';
 export class ClientsComponent implements OnInit {
 
   clients: Client[];
+  totalOwed: number;
 
   constructor (public clientService: ClientService) {
-
   }
 
   ngOnInit () {
     console.log('ngOnInit');
     this.clientService.getClients().subscribe(clients => {
       this.clients = clients;
+      this.getTotalOwed();
       console.log(this.clients);
     });
   }
 
+  getTotalOwed () {
+    if (this.clients && this.clients.length) {
+      const objTotal = this.clients.reduce((a: Client, b: Client) => ({balance: a.balance * 1 + b.balance * 1}));
+      this.totalOwed = objTotal.balance;
+    } else {
+      this.totalOwed = 0;
+    }
+
+    console.log(this.totalOwed);
+  }
 }
