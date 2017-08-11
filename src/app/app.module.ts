2f31b37829b235fd6c765b9c4dc9d68e9c247899
Luis Pintado <luchopintado@gmail.com>
2017-08-11 17:45:35 -0500

    AngularFire2 Setup and Client Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+// Component Imports
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientsComponent } from './components/clients/clients.component';
@@ -15,11 +21,22 @@ import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+// Service Imports
+import { ClientService } from './services/client.service';
+
 const appRoutes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }
 ];
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAzSx1UlpuVU_2cHpULXbUzFKW6Lxnl_Ek',
+  authDomain: 'clientpanel-93da7.firebaseapp.com',
+  databaseURL: 'https://clientpanel-93da7.firebaseio.com',
+  storageBucket: '',
+  messagingSenderId: '43496918221'
+};
 
 @NgModule({
   declarations: [
@@ -38,9 +55,13 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/Client';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+  styleUrls: ['./clients.component.css'],
+  providers: [ClientService]
 })
 export class ClientsComponent implements OnInit {
 
-  constructor() { }
+  clients: Client[];
 
-  ngOnInit() {
+  constructor (public clientService: ClientService) {
+
+  }
+
+  ngOnInit () {
+    console.log('ngOnInit');
+    this.clientService.getClients().subscribe(clients => {
+      this.clients = clients;
+      console.log(this.clients);
+    });
   }
 
 }
